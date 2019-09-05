a6d96fd0c223cf89231106151421e7634faca692
Rosa  Petursdottir <rosapetursdottir@Rosas-MacBook-Air-2.local>
2019-09-05 10:26:55 +0000

    Algorithm written for max_int

---
+++ Documents/forritun/max_int.py/Algorithm written for max_int.py
@@ -0,0 +1,6 @@
+Skref 1: skilgreina max_int og setja það sem lægstu mögulegu töluna sem max_int getur verið = 0
+Skref 2: Skrifa upp forritið: 
+    Þarf að passa að á meðan inputtið sé hærra en 0 muni forritið lesa inputtið (while lykkja)
+    Ef inputtið er hærra en max_num verður það að nýja max_num
+    svo viljum við geta sett inn nýtt input og halda áfram í while lykkjunni 
+    Ef inputtið verður hinsvegar negatíf tala dettum við úr while lykkjunni og prentum max_intið 
